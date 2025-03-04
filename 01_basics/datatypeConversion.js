270c0ca28f147aae02cc5b8bc74b60b256079884
Kunal070702 <kunal.fengade07@gmail.com>
2025-03-04 23:12:54 +0530

    datatype conversion

---
+++ 01_basics/datatypeConversion.js
@@ -0,0 +1,14 @@
+let score = 7;
+
+//console.log(typeof score)
+
+let inString= String(score);
+
+//console.log(typeof score)
+
+let name="kunal"
+
+let inNum=Number(name)
+
+console.log(typeof inNum);
+
